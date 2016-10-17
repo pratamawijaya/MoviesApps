c9bff225fa1094021af100308b5dccfb9943d306
pratamawijaya <set.mnemonix@gmail.com>
2016-10-17 10:48:05 +0700

    add presenter impl test

---
+++ app/src/test/java/pratamawijaya/moviesinfokotlin/presentation/ui/home/presenter/HomePresenterImplTest.java
@@ -0,0 +1,14 @@
+package pratamawijaya.moviesinfokotlin.presentation.ui.home.presenter;
+
+import org.junit.Before;
+
+/**
+ * Created by Pratama Nur Wijaya
+ * Date : Oct - 10/15/16
+ * Project Name : MoviesInfoKotlin
+ */
+public class HomePresenterImplTest {
+  @Before public void setUp() throws Exception {
+
+  }
+}
